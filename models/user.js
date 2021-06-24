f8f5a860a4ab0bee093c30a5518974f4c3cca285
Zsolt Petho <zsoltpetho.it@gmail.com>
2021-06-24 11:22:38 +0200

    adding schema and validating

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 225,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 225,
+    unique: true,
+  },
+  password: {
+    type: String,
+    min: 8,
+    max: 1024,
+  },
+});
+
+const complexityOptions = {
+  min: 10,
+  max: 30,
+  lowerCase: 1,
+  upperCase: 1,
+  numeric: 1,
+  symbol: 1,
+  requirementCount: 2,
+};
+
+const validateUser = (user) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(255).required(),
+    email: Joi.string().min(3).max(255).required().email(),
+    password: passwordComplexity(complexityOptions),
+  });
+  return schema.validate();
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports.userSchema = userSchema;
+module.exports.validateUser = validateUser;
+module.exports.userModel = userModel;
